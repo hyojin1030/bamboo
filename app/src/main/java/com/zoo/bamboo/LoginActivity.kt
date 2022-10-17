e91db8fcb9d92c951f344259e9e2a0f4360df298
hyojin1030 <hyojin_030@naver.com>
2022-10-17 15:57:53 +0900

    구글로그인

---
+++ app/src/main/java/com/zoo/bamboo/LoginActivity.kt
@@ -0,0 +1,118 @@
+package com.zoo.bamboo
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.zoo.bamboo.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private val TAG = "com.zoo.bamboo"
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setResultSignUp()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        checkAccount()
+
+        val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.GOOGLE_CLIENT_ID)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.btnLoginGoogle.setOnClickListener(View.OnClickListener {
+            signIn()
+        })
+
+    }
+
+    private fun setResultSignUp() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                // 정상적으로 결과가 받아와진다면 조건문 실행
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val task: Task<GoogleSignInAccount> =
+                        GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    handleSignInResult(task)
+
+                }
+            }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account.idToken!!)
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("failed", "signInResult:failed code=" + e.statusCode)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) { //성공
+                    val user = mAuth.currentUser
+                    user?.let {
+
+                        val name = user.displayName
+                        val email = user.email
+                        val displayName = user.displayName
+                        val photoUrl = user.photoUrl
+                        val emailVerified = user.isEmailVerified
+                        val uid = user.uid
+                        Log.d("xxxx name",name.toString())
+                        Log.d("xxxx email",email.toString())
+                        Log.d("xxxx displayName",email.toString())
+
+                        checkAccount()
+                    }
+
+                } else { //실패
+                    Log.d("xxxx ","signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+
+    private fun checkAccount() {
+        if (mAuth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
+        resultLauncher.launch(signInIntent)
+    }
+}
