28281e499a043d0d477f08bb791cec857447a512
hyojin1030 <hyojin_030@naver.com>
2022-10-17 17:28:41 +0900

    화면구현

--- app/src/main/java/com/zoo/bamboo/MainActivity.kt
+++ app/src/main/java/com/zoo/bamboo/MainActivity.kt
@@ -2,10 +2,24 @@ package com.zoo.bamboo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import com.zoo.bamboo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.drawerMenu.layoutMenu.openDrawer(Gravity.LEFT)
+
+
+    }
+
+    fun onClickMenu(view: View) {
     }
 }
